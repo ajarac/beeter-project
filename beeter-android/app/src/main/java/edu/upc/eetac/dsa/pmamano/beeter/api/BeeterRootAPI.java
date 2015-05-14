aa84b34e05950302f7c3a19dc7dda2059d8c816c
angel <angel@angel-TravelMate-B113>
2015-05-14 11:58:11 +0200

    l

---
+++ beeter-android/app/src/main/java/edu/upc/eetac/dsa/pmamano/beeter/api/BeeterRootAPI.java
@@ -0,0 +1,18 @@
+package edu.upc.eetac.dsa.pmamano.beeter.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeeterRootAPI {
+
+    private Map<String, Link> links;
+
+    public BeeterRootAPI() {
+        links = new HashMap<String, Link>();
+    }
+
+    public Map<String, Link> getLinks() {
+        return links;
+    }
+
+}
---
+++ beeter-android/app/src/main/java/edu/upc/eetac/dsa/pmamano/beeter/api/MediaType.java
@@ -0,0 +1,6 @@
+package edu.upc.eetac.dsa.pmamano.beeter.api;
+
+public interface MediaType {
+    public final static String BEETER_API_STING = "application/vnd.beeter.api.sting+json";
+    public final static String BEETER_API_STING_COLLECTION = "application/vnd.beeter.api.sting.collection+json";
+}
